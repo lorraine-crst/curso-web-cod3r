44cb7eb94155095d0dd28dd5b54dbde2265986e8
lorraine.silva <89432049+lorraine-crst@users.noreply.github.com>
2023-04-26 11:17:38 -0300

    Aula sobre objetos *IMPORTANTE*

---
+++ Secção 4/objeto(importante).js
@@ -0,0 +1,14 @@
+//mudando dinamicamente os valores de um objeto
+const prod1 = {} // {} uma forma literal de serum objeto
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+
+console.log(prod2)

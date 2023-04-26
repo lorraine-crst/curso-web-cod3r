2c925b4ec92c8ec3eb552a7731e3405481ef55b7
lorraine.silva <89432049+lorraine-crst@users.noreply.github.com>
2023-04-26 18:19:23 -0300

    Aula sobre notação ponto *IMPORTANTE*

---
+++ Secção 4/notacaoPonto(importante).js
@@ -0,0 +1,21 @@
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola'
+// obj1['nome'] = 'Bola2'
+console.log(obj1.nome)
+
+
+//importante THIS = atributo publico, pega a funcao
+function Obj(nome) {
+    this.nome = nome
+    this.exec = function() {
+        console.log('Exec...')
+    }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj3.exec()

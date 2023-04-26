03bca018a6395f9b30211848584ba144c479c555
lorraine.silva <89432049+lorraine-crst@users.noreply.github.com>
2023-04-26 18:19:02 -0300

    Aula sobre hoisting *IMPORTANTE*

---
+++ Secção 4/hoisting(importante).js
@@ -0,0 +1,8 @@
+console.log('a =', a)
+var a = 2
+console.log('a =', a) // a variavel a joga o valor pra cima, que ela existia mes estava undefined, e depois retrna a = 2
+
+
+console.log('b =', b)
+let b = 2
+console.log('b =', b) //declarado com let, o escopo respeita e da erro, dizendo que nao consegue acessar b, o efeito de içamento declarando com let nao acontece

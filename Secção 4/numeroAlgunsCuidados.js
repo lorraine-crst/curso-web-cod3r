86f8abcc215ee1150314204f1c202ae02d832b66
lorraine.silva <89432049+lorraine-crst@users.noreply.github.com>
2023-04-26 11:16:37 -0300

    Aula sobre alguns cuidados com o Number

---
+++ Secção 4/numeroAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) // geraria infinito, nao erro
+console.log("10" / 2) // numeros inteiros funcionam, ele pega o numero dentro da string e divide
+console.log('3' + 2) // string ganha!
+console.log('3' - 2)
+console.log("Show!" * 2) // not a number NaN
+console.log(0.1 + 0.7)
+// console.log(10.toString()) da erro pois não consegue transformar um numero em string teria que ficar assim console.log((10).toFixed(2))
+console.log((10.345).toFixed(2))
